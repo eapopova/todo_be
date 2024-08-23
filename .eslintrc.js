518d17256ddb70494f23163ea35de9b9a18f4245
EMVitmaer <e.vitmaer@tg.dunice.net>
2024-08-23 12:30:08 +0300

    initialized default nest project

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'indent': ['error', 2],
+      'max-len': ['error', { 
+        'code': 80, 
+        'tabWidth': 2, 
+        'ignoreUrls': true,
+        'ignoreStrings': true,
+        'ignoreComments': false,
+      }],
+      'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': false }],
+      'operator-linebreak': ['error', 'before'],
+      'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1, 'maxBOF': 0 }],
+      'space-infix-ops': ['error'],
+  },
+};
