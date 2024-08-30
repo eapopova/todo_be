4833889014e183a1dc4febea2540121c541ab953
EMVitmaer <e.vitmaer@tg.dunice.net>
2024-08-30 11:28:04 +0300

    feat: delete replace a lot of whitespace during the creation of a task

--- src/tasks/dto/createTaskDTO.ts
+++ src/tasks/dto/createTaskDTO.ts
@@ -3,9 +3,8 @@ import { IsBoolean, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateTaskDTO {
 
-  @MaxLength(255)
   @Transform(({ value }) => value.trim())
-  @Transform(({ value }) => value.replace(/\s\s+/g, ' '))
+  @MaxLength(255)
   @IsString()  
   @IsNotEmpty()
     text: string;
