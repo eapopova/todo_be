b2d01972fbd3aafa6b91559d4f9bb5331b74aa53
eapopova <e.popova@dunice.net>
2025-03-03 16:46:55 +0300

    hide client url

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { ConfigService } from "@nestjs/config";
+import { ValidationPipe } from "@nestjs/common";
 
-import { AppModule } from './app.module';
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService)
+  const configService = app.get(ConfigService);
 
-  app.enableCors ({
-    origin: 'http://127.0.0.1:5500'
-  })
-  app.useGlobalPipes(new ValidationPipe({ transform: true }))
+  app.enableCors({
+    origin: configService.get("CLIENT_URL"),
+  });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(configService.get('PORT'));
+  await app.listen(configService.get("PORT"));
 }
 bootstrap();
